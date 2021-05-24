2f0da5a4ab347abeb8b8a7d085ba7c85893d40cb
Kenneth Doan <kennethxdoan@gmail.com>
2021-05-23 17:37:01 -0700

    Attempt

--- database/index.js
+++ database/index.js
@@ -1,3 +1,22 @@
 // https://www.npmjs.com/package/mysql
 
 // TODO: Establish database connection
+
+var mysql = require('mysql');
+var db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'ShopDB'
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log('Connection to MySQL Unsuccessful', err);
+    return;
+  } else {
+    console.log('Successfully Connected to MySQL');
+  }
+})
+
+module.exports = db;
